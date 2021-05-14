caf1fca80de70ec31126925ab360ac05367d7628
mabojie <mabojie@bupt.edu.cn>
2021-05-14 22:15:34 +0800

    20210514login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-function App() {
-  return <div>
-    
-  </div>
+import React, { Component } from 'react'
+import { Button } from 'antd'
+import Login from '../src/pages/Login/Login'
+class app extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <>
+        <Login></Login>
+        {/* <Button type="primary">123</Button> */}
+        
+      </>
+    );
+  }
 }
 
-export default App;
+export default app;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Login.less'
+function Login() {
+  return (
+    <div className="login">
+      <div className='login-header'></div>
+      <div className='user'></div>
+    </div>
+  )
+}
+
+export default Login;
