3e8f0a763a805cd75ff3b27ca3074cbf469013fc
18749817939 <mabojie@bupt.edu.cn>
2021-06-20 19:17:31 +0800

    添加功能

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -4,18 +4,20 @@ import getTime from '../../utils/getTime'
 import { NavLink } from 'react-router-dom'
 import request from '../../api/ajax'
 import { useHistory } from 'react-router-dom'
-import { Card, Button, Table, Spin, message, Modal, Form, Input } from 'antd'
+import { Card, Button, Table, Spin, message, Modal, Form, Input, Select } from 'antd'
 // import { PlusOutlined } from '@ant-design/icons'
 import storage from '../../utils/storage'
+const { Option } = Select;
 function User() {
   const [users, setUsers] = useState([])
   const [isLoading, setisLoading] = useState(true)//判断是否获取到hooks中的请求数据
   const [total, setTotal] = useState(0)
   const [pageNum, setPageNum] = useState(1)
   const [roles, setRoles] = useState({})
+  const [options, setOptions] = useState([])
   const [isModalVisible, setIsModalVisible] = useState(0);
   let history = useHistory()
-  const {form} = Form.useForm
+  const [form] = Form.useForm()
   const getMap = (response) => {
     if (response.status === 0) {
       const arr = response.data.users.map(item => {
@@ -35,7 +37,9 @@ function User() {
       message.error('获取列表失败')
     }
   }
+  //获取列表并对格式进行处理存放在hooks中供后期使用
   const getUsers = async () => {
+    setisLoading(true)
     const response = await request(`/manage/user/list`)
     const { roles, users } = response.data
     if (response.status === 0) {
@@ -43,29 +47,37 @@ function User() {
         pre[role._id] = role.name ? role.name : ""
         return pre
       }, [])
+      const options = roles.map(role => ({ _id: role._id, name: role.name }))
+      setOptions(options)
       setRoles(roleNames)
       getMap(response)
     } else {
       message.error("获取角色列表失败")
-      // request("http://120.55.193.14:5000/manage/user/" + (user._id ? 'update' : 'add'), user, "POST")
+      // request('http://120.55.193.14:5000/manage/user/add'), user, "POST")
     }
   }
-  const onFinish = values=>{
+  const onFinish = async (values) => {
     console.log(values)
+    const creat_time = Date.now()
+    const user = { ...values, creat_time }
+    console.log(user)
+    const response = await request('http://120.55.193.14:5000/manage/user/add', values, "POST")
+    if (response.status === 0) {
+      setTimeout(() => {
+        getUsers()
+      }, 500)
+    } else {
+      message.error(`'添加失败`)
+    }
     form.resetFields()
     setIsModalVisible(0)
-
   }
   const alterUser = user => {
+    console.log(user)
     setIsModalVisible(2)
   }
   const addUser = () => {
     setIsModalVisible(1)
-  }
-  const handleOk = () => {
-    // onFinish()
-    setIsModalVisible(0)
-
   }
   const handleCancel = () => {
     setIsModalVisible(0)
@@ -112,8 +124,8 @@ function User() {
       title: '操作',
       render: (user) =>
         <div >
-          <Button className='user-link' onClick={()=>alterUser(user)} type='link' >修改</Button>
-          <Button className='user-link'  type='link' >删除</Button>
+          <Button className='user-link' onClick={() => alterUser(user)} type='link' >修改</Button>
+          <Button className='user-link' type='link' >删除</Button>
         </div>
     },
   ];
@@ -138,10 +150,10 @@ function User() {
             }}
           />
         </Card>
-        <Modal title="创建用户" visible={isModalVisible != 0 ? true : false}
-          onOk={handleOk} onCancel={handleCancel} footer={null}
+        <Modal title="创建用户" visible={isModalVisible === 0 ? false : true}
+          onCancel={handleCancel} footer={null}
         >
-          <Form labelCol={{span:4}} onFinish={onFinish}>
+          <Form labelCol={{ span: 4 }} onFinish={onFinish} form={form}>
             <Form.Item
               label="用户名："
               className='user-username' name="username"
@@ -151,59 +163,55 @@ function User() {
                   message: 'Please input 用户名!',
                 },
               ]}
+              initialValue=''
             >
-              <Input  style={{ width: '230px' }} placeholder="请输入用户名" />
+              <Input style={{ width: '230px' }} placeholder="请输入用户名" />
             </Form.Item>
-            <Form.Item
-              label="密码："
-              className='user-password' name="password"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input 密码!',
-                },
-              ]}
-            >
-              <Input name="test1" style={{ width: '230px' }} placeholder="请输入密码" />
-            </Form.Item>
+            {
+              isModalVisible === 1 ? <Form.Item
+                label="密码："
+                className='user-password' name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input 密码!',
+                  },
+                ]}
+              >
+                <Input name="test1" style={{ width: '230px' }} placeholder="请输入密码" />
+              </Form.Item> : ''
+            }
             <Form.Item
               label="手机号："
               className='user-phone' name="phone"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input 手机号!',
-                },
-              ]}
             >
               <Input name="test1" style={{ width: '230px' }} placeholder="请输入手机号" />
             </Form.Item>
             <Form.Item
               label="邮箱："
               className='user-email' name="email"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input 邮箱!',
-                },
-              ]}
             >
               <Input name="test1" style={{ width: '230px' }} placeholder="请输入邮箱" />
             </Form.Item>
             <Form.Item
               label="角色："
-              className='user-role' name="role"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input 角色!',
-                },
-              ]}
+              className='user-role' name="role_id"
             >
-              <Input name="test1" style={{ width: '230px' }} placeholder="请输入角色" />
+              <Select style={{ width: '230px' }}
+                placeholder="请选择角色"
+              >
+                {
+                  options.map(item => <Option key={item._id}>
+                    {
+                      item.name
+                    }
+                  </Option>)
+                }
+              </Select>
             </Form.Item>
             <Form.Item>
-              <Button type="primary" htmlType="submit">确认</Button>
+              <Button type="primary" htmlType="submit"  >确认</Button>
+              <Button type="primary" htmlType="button" onClick={handleCancel} >取消</Button>
             </Form.Item>
           </Form>
         </Modal>
